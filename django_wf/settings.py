9ca99570e8c8e95151fbf81f483e499705d5296a
marcinadd <marcinadd@gmail.com>
2020-03-18 20:34:20 +0100

    Dodanie aplikacji wyniki

--- django_wf/settings.py
+++ django_wf/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'wyniki.apps.WynikiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
