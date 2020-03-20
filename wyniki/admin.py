62bc13f374a282aefbfa51ee12886cf5b27c7c43
marcinadd <marcinadd@gmail.com>
2020-03-20 14:35:26 +0100

    Rejestracja klas w panelu i dodanie metod str

--- wyniki/admin.py
+++ wyniki/admin.py
@@ -1,1 +1,8 @@
-# Register your models here.
+from django.contrib import admin
+
+from wyniki.models import Class, Student, Sport, Result
+
+admin.site.register(Class)
+admin.site.register(Student)
+admin.site.register(Sport)
+admin.site.register(Result)
--- wyniki/models.py
+++ wyniki/models.py
@@ -5,23 +5,39 @@ class Class(models.Model):
     name = models.CharField(max_length=5)
     year = models.SmallIntegerField()
 
+    def __str__(self):
+        return self.name + " " + str(self.year)
+
+
 class Student(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     clazz = models.ForeignKey(Class, on_delete=models.SET_NULL, null=True)
 
+    def __str__(self):
+        return self.first_name + " " + self.last_name + " " + str(self.clazz)
+
+
 class Sport(models.Model):
     class Unit(models.TextChoices):
-        KILOGRAM = 'kg', ('kilogram')
-        METER = 'm', ('metr')
-        SECOND = 's', ('sekunda')
-        NONE = '', ('')
+        KILOGRAM = 'kg', 'kilogram'
+        METER = 'm', 'metr'
+        SECOND = 's', 'sekunda'
+        NONE = '', ''
+
     name = models.CharField(max_length=40)
     unit = models.CharField(max_length=2, choices=Unit.choices, default=Unit.NONE)
     more_better = models.BooleanField()
+
+    def __str__(self):
+        return self.name
+
 
 class Result(models.Model):
     value = models.DecimalField(max_digits=4, decimal_places=2)
     student = models.ForeignKey(Student, on_delete=models.SET_NULL, null=True)
     sport = models.ForeignKey(Sport, on_delete=models.SET_NULL, null=True)
     date = models.DateTimeField()
+
+    def __str__(self):
+        return str(self.value) + " " + self.sport.unit + " " + str(self.sport) + " " + str(self.student)
